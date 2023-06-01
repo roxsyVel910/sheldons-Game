9295efc6872763efe87ac7faa8063e0892e66e43
Roxsy <roxsy.dj@gmail.com>
2023-06-01 08:21:01 -0500

    recorriendo array de opciones

--- src/Game.jsx
+++ src/Game.jsx
@@ -11,14 +11,32 @@ function Game() {
     { id: 4, name: "Spock", emoji: "🖖", beats: [3, 0] },
   ];
 
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [userMessage, setUserMessage] = useState(null);
+  const [computerMessage, setComputerMessage] = useState(null);
+  const [result, setResult] = useState(null);
+  const [disabled, setDisabled] = useState(false);
  
+  const handlePlay = (userChoice) => {};
 
   return (
     <>
       <div className="flex items-center justify-center h-screen bg-gray-800">
       <div className="rounded-lg p-4 bg-gray-500">
         <h1 className="text-3xl mb-4 text-center font-bold">¡A jugar!</h1>
-       
+       <div className="max-w-md mx-auto">
+          {options.map((option) => (
+            <button
+              key={option.id}
+              option={option}
+              handlePlay={handlePlay}
+              disabled={disabled}
+            >
+            {option.emoji}
+            </button>
+          ))}
+          </div>
       </div>
     </div>
     </>
