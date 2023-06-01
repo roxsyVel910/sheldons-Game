a40392070baa1b9b8fccd501622e5e2f095f2d7c
Roxsy <roxsy.dj@gmail.com>
2023-06-01 07:22:23 -0500

    array de opciones

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 function Game() {
+
+  const options = [
+    { id: 0, name: "Rock", emoji: "🪨", beats: [2, 3] },
+    { id: 1, name: "Paper", emoji: "📄", beats: [0] },
+    { id: 2, name: "Scissors", emoji: "✂️", beats: [1, 3] },
+    { id: 3, name: "Lizard", emoji: "🦎", beats: [1] },
+    { id: 4, name: "Spock", emoji: "🖖", beats: [3, 0] },
+  ];
+
  
 
   return (
